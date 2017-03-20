f5de30bdd4be81c0d1553c84acb0e739ad371b48
jamiex304 <jamiex304@hotmail.com>
2017-03-20 20:54:39 +0000

    Block Breaker V_0.1
    -Basic Level system
    -Music Player (plays music across all levels)
    -Temp buttons for navigation

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//Level Manager script is called within the GameObject in a level called Level Manager (Level Manager object is a empty GameObject)
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string levelName){
+		Debug.Log("Level Load Requested for: " + levelName);
+		Application.LoadLevel(levelName); //Loads a level
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Quit Requested for Game");
+		Application.Quit(); //Works for console, PC and final build
+	}
+	
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	//Variables
+	static MusicPlayer instance = null;
+	// Use this for initialization
+	void Start () {
+		if (instance != null){
+				Destroy (gameObject);
+				//Print statement is for logging purposes
+				print ("Music player exists destorying self");
+		}else {
+				instance = this;
+				GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
